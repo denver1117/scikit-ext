3c480d16fd86dee095dbc40ae51b00e43cfe9182
denver1117 <emitchellh@gmail.com>
2017-09-29 22:25:37 +0000

    Add custom norm functionality

--- scikit_ext/extensions.py
+++ scikit_ext/extensions.py
@@ -4,6 +4,7 @@ Various scikit-learn extensions
 
 import numpy as np
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.preprocessing import normalize
 
 class OneVsRestAdjClassifier(OneVsRestClassifier):
     """
@@ -17,10 +18,45 @@ class OneVsRestAdjClassifier(OneVsRestClassifier):
     knowledge about the class by inspecting its corresponding classifier. 
     This is the most commonly used strategy for multiclass classification and is a fair default choice.
 
-    The adjusted version is a custom extension which overwrites the inherited predict_proba() method 
-    and returns the pre-normalized per-class predicted probabilities, rather than forcing them to sum
-    to 1.  All other methods are inherited from OneVsRestClassifier.
+    The adjusted version is a custom extension which overwrites the inherited predict_proba() method with
+    a more flexible method allowing custom normalization for the predicted probabilities. Any norm
+    argument that can be passed directly to sklearn.preprocessing.normalize is allowed. Additionally,
+    norm=None will skip the normalization step alltogeter. To mimick the inherited OneVsRestClassfier
+    behavior, set norm='l2'. All other methods are inherited from OneVsRestClassifier.
+
+
+    Parameters	
+    ----------
+    estimator : estimator object
+        An estimator object implementing fit and one of decision_function or predict_proba.
+    n_jobs : int, optional, default: 1
+        The number of jobs to use for the computation. If -1 all CPUs are used. 
+        If 1 is given, no parallel computing code is used at all, which is useful for debugging. 
+        For n_jobs below -1, (n_cpus + 1 + n_jobs) are used. Thus for n_jobs = -2, all CPUs but one are used.
+    norm: str, optional, default: None
+        Normalization method to be passed straight into sklearn.preprocessing.normalize as the norm 
+        input. A value of None (default) will skip the normalization step.
+
+    Attributes
+    ----------
+    estimators_ : list of n_classes estimators
+        Estimators used for predictions.
+    classes_ : array, shape = [n_classes]
+        Class labels.
+    label_binarizer_ : LabelBinarizer object
+        Object used to transform multiclass labels to binary labels and vice-versa.
+    multilabel_ : boolean
+        Whether a OneVsRestClassifier is a multilabel classifier.
     """
+
+    def __init__(self, estimator, norm=None, **kwargs):
+        """ 
+        Additional norm argument along with inheritance
+        """
+
+        OneVsRestClassifier.__init__(
+            self, estimator, **kwargs)
+        self.norm = norm
  
     def predict_proba(self, X):
         """ 
@@ -42,6 +78,10 @@ class OneVsRestAdjClassifier(OneVsRestClassifier):
         probs = []
         for index in range(len(self.estimators_)):
             probs.append(self.estimators_[index].predict_proba(X)[:,1])            
-        return np.array([
+        out = np.array([
             [probs[y][index] for y in range(len(self.estimators_))] 
             for index in range(len(probs[0]))])
+        if self.norm:
+            return normalize(out, norm=self.norm)
+        else:
+            return out
--- test_ovr_adj.py
+++ test_ovr_adj.py
@@ -19,28 +19,36 @@ def main():
 
     # initialize models
     dt = DecisionTreeClassifier(max_depth=1)
-    ovr_scikit = OneVsRestClassifier(dt)
-    ovr_adj = OneVsRestAdjClassifier(dt)
+    ovr_scikit = OneVsRestClassifier(dt, n_jobs=2)
+    ovr_adj = OneVsRestAdjClassifier(dt, n_jobs=2)
+    ovr_adj_norm = OneVsRestAdjClassifier(dt, norm="l1", n_jobs=2)
 
     # fit models
     ovr_scikit.fit(X_train, y_train)
     ovr_adj.fit(X_train, y_train)
+    ovr_adj_norm.fit(X_train, y_train)
 
     # print scores (should be equal)
     print ovr_scikit.score(X_test, y_test)
     print ovr_adj.score(X_test, y_test)
+    print ovr_adj_norm.score(X_test, y_test)
 
     # get probs from predict_proba() methods
     probs_scikit = ovr_scikit.predict_proba(X_test)
     probs_adj = ovr_adj.predict_proba(X_test)
+    probs_adj_norm = ovr_adj_norm.predict_proba(X_test)
 
     # print types (should be equal)
     print type(probs_scikit)
     print type(probs_adj)
+    print type(probs_adj_norm)
 
-    # print probability matrices (should be slightly different)
+    # print probability matrices 
+    # (first and third should be equal
+    # second should differ slightly)
     print probs_scikit
     print probs_adj
+    print probs_adj_norm
 
 if __name__ == "__main__":
     main()
